5638f702635dd87b33ba6354b21df76241e7c4bc
Max Du <maxdubusiness@gmail.com>
2017-11-07 23:04:03 -0500

    added definitions

--- train.py
+++ train.py
@@ -1,3 +1,7 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+import tensorflow as tf
 def variable_summaries(var):
   """Attach a lot of summaries to a Tensor (for TensorBoard visualization)."""
   with tf.name_scope('summaries'):
